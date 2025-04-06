2b1a2ee2344859398d2d44b2e52d4016a1d1ff33
Panshulgithub <panshul.sadana@outlook.com>
2025-04-06 16:18:40 +0530

    Add new C Program

---
+++ Multiplication.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+    int num ,i ;
+    printf("Enter the number for Multiplication Table ");
+    scanf("%d", &num);
+
+    printf("Multiplication Table of %d:\n",num);
+    for (i = 1; i <= 20; i++)
+    {
+        printf("%d*%d = %d\n",num,i,num*i);
+    }
+    
+      return 0;
+}
